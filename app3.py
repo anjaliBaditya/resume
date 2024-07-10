a6abde0a40061a041ed7b1210c6c8e3342e50809
anjaliBaditya <anjalibaditya8@gmail.com>
2024-07-11 01:07:06 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import json
 import genai
 
 app = Flask(__name__)
@@ -11,8 +12,13 @@ def upload_json_file():
     if file.filename == '':
         return 'No selected file'
     if file and file.filename.endswith('.json'):
-        md_file = genai.upload_file(path=file, display_name="Starred Repos Content", mime_type="text/markdown")
-        PROMPT="""you are a helper you will be provided a need and you have to search through the data of the starred repos of users and you have to provide the best solution they can use
+        data = file.read()
+        try:
+            json_data = json.loads(data.decode('utf-8'))
+            print(json_data)
+            # Process the JSON data
+            md_file = genai.upload_file(path="test.json", display_name="Starred Repos Content", mime_type="text/markdown")
+            PROMPT="""you are a helper you will be provided a need and you have to search through the data of the starred repos of users and you have to provide the best solution they can use
 using this JSON schema:
 Repositories = [
   {
@@ -29,14 +35,16 @@ Repositories = [
   },.....
 ]
 """
-        model = genai.GenerativeModel(model_name="models/gemini-1.5-flash" , system_instruction=PROMPT, generation_config={"response_mime_type": "application/json"})
-        response = model.generate_content(
-            [
-                "These are my starred repos tell me something i can use in state management",
-                md_file,
-            ]
-        )
-        return jsonify({'response': response.text})
+            model = genai.GenerativeModel(model_name="models/gemini-1.5-flash" , system_instruction=PROMPT, generation_config={"response_mime_type": "application/json"})
+            response = model.generate_content(
+                [
+                    "These are my starred repos tell me something i can use in state management",
+                    md_file,
+                ]
+            )
+            return jsonify({'response': response.text})
+        except json.JSONDecodeError:
+            return 'Invalid JSON file'
     return 'Only JSON files are allowed'
 
 if __name__ == '__main__':
